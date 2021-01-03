08d6b512913ff4dd90e9796c2bc2dac75879dfd4
mikelssa <76864089+mikelssa@users.noreply.github.com>
2021-01-03 08:05:53 +0200

    Update sese.sh

--- sese.sh
+++ sese.sh
@@ -13,14 +13,6 @@ sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
-echo 'deb http://http.kali.org/kali kali-experimental main non-free contrib' | sudo /etc/apt/sources.list 
-echo 'deb http://http.kali.org/kali kali-last-snapshot main non-free contrib' | sudo /etc/apt/sources.list
-echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib' | sudo /etc/apt/sources.list
-echo 'deb http://ftp.de.debian.org/debian' | sudo  /etc/apt/sources.list
-sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/apt/sources.list
-wget https://archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-key.asc
-sudo apt-get update
-sudo apt --fix-broken install
 sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb

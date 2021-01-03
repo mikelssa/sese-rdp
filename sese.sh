cf0b588ab78f47bfe82a4c7dabd9800050c2e5fa
mikelssa <76864089+mikelssa@users.noreply.github.com>
2021-01-03 08:55:17 +0200

    Update sese.sh

--- sese.sh
+++ sese.sh
@@ -18,13 +18,6 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo echo "deb http://http.kali.org/kali kali-experimental main non-free contrib" | sudo tee -a /etc/apt/sources.list 
-sudo echo "deb http://http.kali.org/kali kali-last-snapshot main non-free contrib" | sudo tee /etc/apt/sources.list
-sudo echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list
-sudo echo "deb http://ftp.de.debian.org/debian" | sudo tee /etc/apt/sources.list
-sudo wget https://archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-key.asc
-sudo apt-get update&& sudo apt-get upgrade
-sudo apt --fix-broken install
 sudo adduser mikel chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
